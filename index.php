0bfa0e7f779b565b74b0e69653a7fb5d7d465fde
SoukainaKania <kaniasoukaina33@gmail.com>
2025-11-15 00:16:05 +0100

    Ajout formulaire ajout tâche

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
     <span class="navbar-brand">To-Do List</span>
   </div>
 </nav>
+<form method="post" class="mb-3">
+  <input type="hidden" name="action" value="new">
+  <div class="input-group">
+      <input type="text" name="title" class="form-control" placeholder="Nouvelle tâche" required>
+      <button class="btn btn-success">Ajouter</button>
+  </div>
+</form>

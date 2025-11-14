f5a69e8e284bb2f74900a067fbb30b7db61c67e1
SoukainaKania <kaniasoukaina33@gmail.com>
2025-11-15 00:18:26 +0100

    message

--- index.php
+++ index.php
@@ -10,3 +10,20 @@
       <button class="btn btn-success">Ajouter</button>
   </div>
 </form>
+<ul class="list-group">
+<?php foreach($taches as $t): ?>
+  <li class="list-group-item 
+      <?= $t['done'] ? 'list-group-item-success' : 'list-group-item-warning' ?> 
+      d-flex justify-content-between align-items-center">
+
+      <?= htmlspecialchars($t['title']) ?>
+
+      <form method="post">
+        <input type="hidden" name="id" value="<?= $t['id'] ?>">
+
+        <button name="action" value="toggle" class="btn btn-sm btn-primary">✔</button>
+        <button name="action" value="delete" class="btn btn-sm btn-danger">🗑</button>
+      </form>
+  </li>
+<?php endforeach; ?>
+</ul>

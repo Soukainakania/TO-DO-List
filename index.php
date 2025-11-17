d3d855514afcba47d8fb56ab0c6f42216ff538d8
ZOUHAIR <zouhairmazi@example.com>
2025-11-17 20:10:25 +0100

    Dar lchanges f fichier2

--- index.php
+++ index.php
@@ -4,9 +4,8 @@ define('DB_USER', 'root');
 define('DB_PASS', 'ZOUHAIR');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
     die("Erreur : " . $conn->connect_error);
 }
